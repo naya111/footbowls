41cf35fbbea790e0515b7dd92d347ec18849f915
naya111 <zea2107@columbia.edu>
2023-12-14 20:18:03 +0700

    edited docstrings

--- src/footbowls/footbowls.py
+++ src/footbowls/footbowls.py
@@ -12,6 +12,22 @@ def get_teams(team_names=None, **kwargs):
     - kwargs: additional parameters for the API request.
 
     Returns a pandas dataframe containing the retrieved data.
+
+    ------
+    Example usage for 'get_teams' function with team names
+    >>>> team_names = ['liverpool', 'Real Madrid', 'manchester united']
+    >>>> teams_df = get_teams(team_names=team_names)
+    >>>> print(teams_df)
+        Team ID          Team Name Team Code                 Team Venue  
+    0       40          Liverpool       LIV                    Anfield   
+    1      541        Real Madrid       REA  Estadio Santiago Bernabéu   
+    2       33  Manchester United       MUN               Old Trafford   
+
+    Country of Origin  
+    0           England  
+    1             Spain  
+    2           England  
+
     """
     base_url = "https://api-football-v1.p.rapidapi.com/v3/teams"
 
@@ -64,11 +80,27 @@ def get_players(parameter_id, season_id=None, **kwargs):
     API wrapper for the footbowls API to get player information.
 
     Parameters:
-    - parameter_id: string or list, the player ID(s) to retrieve information.
+    - parameter_id: string or list, the player ID(s) to retrieve information. (Full player ID list available on the API-Football website)
     - season_id: int, the ID corresponding to the season (required for 'players' parameter).
     - kwargs: additional parameters for the API request.
 
     Returns a pandas dataframe containing the retrieved data.
+
+    ------
+    Example usage for 'get_players' function with player IDs and Season ID 2022
+    >>>> player_ids = [77, 56, 290] 
+    >>>> players_df = get_players(player_ids, 2014)
+    >>>> print(players_df)
+
+        Player Name    Position  Total Goals  Nationality  Player DOB  \
+    0          Mats Rits  Midfielder            2      Belgium  1993-07-18   
+    1  Antoine Griezmann    Attacker           22       France  1991-03-21   
+    2    Virgil van Dijk    Defender            4  Netherlands  1991-07-08   
+
+    Current Season Team  
+    0         KV Mechelen  
+    1     Atletico Madrid  
+    2              Celtic  
     """
     base_url = "https://api-football-v1.p.rapidapi.com/v3/players"
 
