994ed0b017f6a171253c9c106428d6b6198f6ee8
naya111 <zea2107@columbia.edu>
2023-12-14 21:22:07 +0700

    fixed test folder

--- tests/test_footbowls.py
+++ tests/test_footbowls.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import pytest
-from footbowls import get_teams, get_players
+from footbowls import footbowls
 
 @pytest.fixture
 def sample_team_names():
     return ['Liverpool', 'Real Madrid', 'Manchester United']
 
 def test_get_teams(sample_team_names):
-    teams_df = get_teams(team_names=sample_team_names)
+    teams_df = footbowls.get_teams(team_names=sample_team_names)
     assert isinstance(teams_df, pd.DataFrame)
-    
+
     expected_columns = ['Team ID', 'Team Name', 'Team Code', 'Team Venue', 'Country of Origin']
     assert all(column in teams_df.columns for column in expected_columns)
 
@@ -23,7 +23,7 @@ def sample_season_id():
     return 2014
 
 def test_get_players(sample_player_ids, sample_season_id):
-    players_df = get_players(sample_player_ids, sample_season_id)
+    players_df = footbowls.get_players(sample_player_ids, sample_season_id)
     assert isinstance(players_df, pd.DataFrame)
 
     expected_columns = ['Player Name', 'Position', 'Total Goals', 'Nationality', 'Player DOB', 'Current Season Team']
